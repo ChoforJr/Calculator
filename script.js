015553b6f7839da3f57fe5b0381b8408741628fd
ChoforJr <choforforsakang@gmail.com>
2024-06-29 02:47:50 +0100

    Added functionality to the various buttons
    if you click the numbers while holding shift it will be input to the
    first space and if you click the numbers while holding ctrl it will be
    input to the second space, I added functionality to the operator buttons
    so if you click them you can see the signs change in real time, also
    made the equalTo button produce the result using the first input, the
    second input and the operator, and finally add a function to the clear
    button so that if you click it all existing data will be wipe starting
    at fresh

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
 
 const add = function(a,b) {
-	return a + b;
+    let firstInput = Number(a);
+    let secondInput = Number(b);
+	return firstInput + secondInput;
 };
 
 const subtract = function(a,b) {
-	return a - b;
+    let firstInput = Number(a);
+    let secondInput = Number(b);
+	return firstInput - secondInput;
 };
 
 const multiply = function(a,b) {
-    return a * b;
+    let firstInput = Number(a);
+    let secondInput = Number(b);
+	return firstInput * secondInput;
 };
 
 const divide = function (a,b){
-    if (a==0){
-        return "You can't divide by 0";
+    let firstInput = Number(a);
+    let secondInput = Number(b);
+    if (firstInput==0){
+        return "Invalid";
     }
-    return a/b;
+    return firstInput / secondInput;
 }
 
-const digits = document.querySelector(".digits");
+const buttonArea = document.querySelector(".buttonArea");
+
+let operator = document.querySelector("#operator");
+
+let fInput = document.querySelector("#fInput");
+let sInput = document.querySelector("#sInput");
+let result = document.querySelector("#result");
+
+document.querySelectorAll('.digits').forEach((button) => {
+    button.addEventListener('click', btnAction);
+});function btnAction(event) {
+    const btn = event.target;
+    if (event.shiftKey) fInput.textContent += btn.innerText;
+    if (event.ctrlKey) sInput.textContent += btn.innerText;
+}
+
+document.querySelectorAll('.signs').forEach((button) => {
+    button.addEventListener('click', btnSignsAction);
+});function btnSignsAction(event) {
+    const btn = event.target;
+    operator.textContent = btn.innerText;
+}
+
+let equalTo = document.querySelector('#equalTo');
+equalTo.addEventListener('click', () =>{
+    if (operator.textContent == '+'){
+        result.textContent= add(fInput.textContent,sInput.textContent);
+    } else if (operator.textContent == '-'){
+        result.textContent= subtract(fInput.textContent,sInput.textContent);
+    } else if (operator.textContent == 'x'){
+        result.textContent= multiply(fInput.textContent,sInput.textContent);
+    } else if (operator.textContent == '÷'){
+        result.textContent= divide(fInput.textContent,sInput.textContent);
+    }
+});
+
+let clear = document.querySelector('#clear');
+clear.addEventListener('click', () =>{
+    fInput.textContent = '';
+    sInput.textContent = '';
+    result.textContent = '';
+});
